c5a09a0f37c487db8655fab6d55e18f97b0588eb
Hunter Carroll <carrollh@msoe.edu>
2025-11-09 14:54:17 -0600

    Part 3: Getting Details from a received signal

--- signal_sigaction.c
+++ signal_sigaction.c
@@ -1,8 +1,44 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_sigaction.c
+ * Modified by: Hunter Carroll
  * 
  * Brief summary of program:
+ * Uses sigaction to register a handler for the SIGUSR1 signal.
+ * Prints process ID of sender when SIGUSR1 is received.
+ * Exits when SIGUSR1 is received 7 times.
  */
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+void handle_signal(int signum) {
+    printf("SIGUSR1 received\n");
+    printf("Process ID: %d\n", getpid());
+    fflush(stdout);
+    sleep(1);
+    return;
+}
+
+int main(int argc, char* argv[]) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handle_signal;
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGUSR1, &sa, NULL);
+    int count = 0;
+    while(1) {
+        sleep(1);
+        printf("Sleeping\n");
+        count++;
+        if(count > 5 && count < 7) {
+            raise(SIGUSR1);
+
+        } else if (count > 7) {
+            exit(0);
+        }
+    }
+    return 0;
+}
 

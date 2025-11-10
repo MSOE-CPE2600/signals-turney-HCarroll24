5a0fbf802b645d91e15d6bc563073fff07cb85f9
Hunter Carroll <carrollh@msoe.edu>
2025-11-10 11:26:38 -0600

    Finished

--- signal_research.c
+++ signal_research.c
@@ -1,11 +1,19 @@
 #include <signal.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int main(void)
 {
     pid_t pid;
     int signal_num;
 
+    pid = getpid();
+    signal_num = SIGUSR1;
+
+    printf("Current process PID: %d\n", pid);
+    printf("Sending signal %d to process %d\n", signal_num, pid);
+    
     kill(pid, signal_num);
 
     return 0;
